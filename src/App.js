d90cb44c3f79bd959cf1c1ff6d76f2a9c9d5797e
davidmichaelm <davemarquardt@gmail.com>
2021-01-26 09:10:32 -0600

    Fetch event data from Azure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import EventTable from "./components/EventTable";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <EventTable />
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            events: []
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <EventTable events={this.state.events}/>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        fetch("https://dmarquardt-modas.azurewebsites.net/api/event/pageSize/10/page/1")
+            .then(r => r.json())
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    events: data.events
+                });
+            });
+    }
 }
 
 export default App;
--- src/components/EventTable.js
+++ src/components/EventTable.js
@@ -2,30 +2,36 @@ import Table from 'react-bootstrap/Table';
 import EventRow from "./EventRow";
 import React from "react";
 
-class EventTable extends React.Component {
-
-    render() {
-        return (
-            <div className="App">
-                <Table>
-                    <thead>
-                    <tr>
-                        <th>Flag</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                        <th>Location</th>
-                    </tr>
-                    </thead>
-                    <EventRow
-                        flagged={"false"}
-                        date={"today"}
-                        time={"now"}
-                        location={"thermostat"}
-                    />
-                </Table>
-            </div>
-        );
-    }
+function EventTable(props) {
+    return (
+        <div className="App">
+            <Table>
+                <thead>
+                <tr>
+                    <th>Flag</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Location</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    props.events.map(event => {
+                        return (
+                            <EventRow
+                                flagged={event.flag ? "true" : "false"}
+                                location={event.loc}
+                                date={event.stamp}
+                                time={event.stamp}
+                                key={event.id}
+                            />
+                        );
+                    })
+                }
+                </tbody>
+            </Table>
+        </div>
+    );
 }
 
 export default EventTable;
