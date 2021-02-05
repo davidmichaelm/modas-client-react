1d3a1f877ecaab7e12e7609d870ac2e2491d13d3
davidmichaelm <davemarquardt@gmail.com>
2021-02-04 19:28:16 -0600

    Responsive page header

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -17,7 +17,8 @@ function PageHeader(props) {
     return (
         <header className="d-flex p-2 px-xl-0 align-items-center container-xl p-0">
             <h2 className="ml-1 ml-lg-0 mb-0">
-                Modas
+                <span class="d-none d-md-inline">Moving Object Detection Alert System</span>
+                <span className="d-inline d-md-none">MODAS</span>
             </h2>
             <div className="ml-auto mb-0 mr-1 h4" style={{cursor: "pointer"}}>
                 <i className="bi bi-gear-fill text-white"  onClick={handleShow}></i>
